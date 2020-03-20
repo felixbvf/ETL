79b3bc61a0a60c15ab6b77076ea5e413af6efade
felixbvf <felixbvf@gmail>
2020-03-20 13:05:39 -0400

    Export gbage, gbdac y gbdoc

--- app/Exports/GbdacSheet.php
+++ app/Exports/GbdacSheet.php
@@ -13,44 +13,169 @@ use Maatwebsite\Excel\Concerns\WithTitle;
 class GbdacSheet implements FromCollection,WithHeadings,ShouldAutoSize,WithMapping,WithTitle
 {
     
-
+    public function collection()
+    {
+        $gbdac = Gbpersona::Select(DB::raw("id_persona, concat_ws(' ',nombre1, nombre2) as nombres ,appaterno,apmaterno,apesposo,email"))->orderByRaw('id_persona::numeric asc')->get();
+        return $gbdac;
+    }
     public function map($gbdac) : array { //Datos a exportar
         return [
             $gbdac->id_persona,
-            $gbdac->nrodoc,
-            $gbdac->fecha_exp,
+            $gbdac->nombres,
+            $gbdac->appaterno,
+            $gbdac->apmaterno,
             '||',
-            $gbdac->fecha_reg,
-            '20',
-            '20',
-            $gbdac->usuario_reg,
-            $gbdac->hora_reg,
             '||',
-            '||'
+            '||',
+            '||',
+            '||',
+            '||',
+            '||', //Ref. Ubic. Domicilio
+            '||',
+            '||',
+            '||',
+            '||',
+            '||',
+            '||',
+            '||', //Fecha de Ingreso al trabajo
+            '||',
+            '||',
+            '||',
+            '||',
+            '||',
+            '||',
+            '||', //Fecha Cambio Estado
+            '||',
+            '||',
+            '||',
+            '||',
+            '||',
+            '||',
+            '||', //Fecha de Evaluacion
+            '||',
+            '||',
+            '||',
+            '||',
+            '||',
+            '||',
+            '||', //Fecha ultima actualizacion
+            '||',
+            '||',
+            $gbdac->nombre1,
+            $gbdac->nombre2,
+            $gbdac->apesposo,
+            '||',
+            '||', //Codigo Nacionalidad (gbconpfij=95)
+            '||',
+            '||',
+            '||',
+            '||',
+            '||', //Actividad (gbconpfij=48)
+            '||',
+            '||',
+            '||',
+            '||',
+            '||',
+            '||',
+            '||', //Ambito Geografico (Pref=17)
+            '||',
+            '||',
+            '||',
+            '||',
+            '||',
+            '||',
+            '||', // Nro de Familiares
+            '||',
+            '||',
+            '||',
+            '||',
+            '||',
+            '||',
+            '||', //Sin Uso
+            '||'          
+           
         ] ;
     }
 
     public function headings(): array  //Encabezado de excel
     {
         return [
-            'gbdoccage', //codigo agenda
-            'gbdocndid', //numero de identidad
-            'gbdocfvid', // fecha vencimiento CI
-            'gbdocnruc',
-            'gbdocfreg',  //fecha de registro
-            'gbdocplaz', //Plaza = 20
-            'gbdocagen', //Agencia
-            'gbdocuser',
-            'gbdochora',
-            'gbdocfpro',
-            'gbdocfemi',
+            'GBDACCAGE', // Codigo de agenda
+            'GBDACNOMB', // Nombre del cliente
+            'GBDACAPE1', // Apellido apterno
+            'GBDACAPE2', //Apellido materno
+            'GBDACNOCO',
+            'GBDACCOCL',
+            'GBDACREFP',
+            'GBDACMAIL',
+            'GBDACCIUD',
+            'GBDACUBID',
+            'GBDACREFD', //Ref. Ubic. Domicilio
+            'GBDACNEMP',
+            'GBDACCCAR',
+            'GBDACNINT',
+            'GBDACCIUO',
+            'GBDACUBIO',
+            'GBDACREFO',
+            'GBDACFING', //Fecha de Ingreso al trabajo
+            'GBDACRSEG',
+            'GBDACTRES',
+            'GBDACNORI',
+            'GBDACFCNA',
+            'GBDACFLLE',
+            'GBDACSTAT',
+            'GBDACFSTA', //Fecha Cambio Estado
+            'GBDACSANT',
+            'GBDACFANT',
+            'GBDACCNAC',
+            'GBDACMENS',
+            'GBDACTMEN',
+            'GBDACNEVA',
+            'GBDACFEVA', //Fecha de Evaluacion
+            'GBDACCONY',
+            'GBDACNIVE',
+            'GBDACCAND',
+            'GBDACCLFA',
+            'GBDACFCAA',
+            'GBDACFCAL',
+            'GBDACFUAD', //Fecha ultima actualizacion
+            'GBDACCELU',
+            'GBDACCLAS',
+            'GBDACNOM1', // Primer nombre
+            'GBDACNOM2', // Segundo nombre
+            'GBDACAPE3', // Apellido de esposo
+            'GBDACTMTO',
+            'GBDACCNCN', //Codigo Nacionalidad (gbconpfij=95)
+            'GBDACCNCO',
+            'GBDACPAIP',
+            'GBDACRUBR',
+            'GBDACSECT',
+            'GBDACACTV', //Actividad (gbconpfij=48)
+            'GBDACCLSC',
+            'GBDACTIPO',
+            'GBDACPWEB',
+            'GBDACOBSV',
+            'GBDACREPR',
+            'GBDACCICL',
+            'GBDACAMBG', //Ambito Geografico (Pref=17)
+            'GBDACIDIP',
+            'GBDACIDIS',
+            'GBDACCIUN',
+            'GBDACPESO',
+            'GBDACESTT',
+            'GBDACFMAT',
+            'GBDACNFAM', // Nro de Familiares
+            'GBDACCALP',
+            'GBDACLACT',
+            'GBDACFIAC',
+            'GBDACTAAC',
+            'GBDACTMAC',
+            'GBDACTDAC',
+            'GBDACIDAC', //Sin Uso
+            'GBDACDISC',
         ];
     }
-    public function collection()
-    {
-        $gbdac = Gbpersona::Select(DB::raw("id_persona, nombre1+' '+ nombre2 ,appaterno,apmaterno,email::timestamp::date,usuario_reg,fecha_reg::timestamp::time as hora_reg"))->orderByRaw('id_persona::numeric asc')->get();
-        return $gbdac;
-    }
+
 
     public function title(): string
     {
--- app/Exports/PersonasExport.php
+++ app/Exports/PersonasExport.php
@@ -6,13 +6,16 @@ use Maatwebsite\Excel\Concerns\WithMultipleSheets;
 
 
 class PersonasExport implements WithMultipleSheets
-{
+{  
     use Exportable;
     public function sheets(): array
-    {
+    {   ini_set('memory_limit', '-1');
+        ini_set('max_execution_time', '-1');
+        ini_set('max_input_time', '-1');
+        set_time_limit('-1');
         $sheets = [];
         $sheets[] = new GbageSheet();
-      //  $sheets[] = new GbdacSheet(); 
+        $sheets[] = new GbdacSheet(); 
         $sheets[] = new GbdocSheet(); 
         return $sheets;
     }
